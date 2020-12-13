dc70a58d7dce68bc7d414709978389e6529987de
HuffDaddy0 <ty.huff24@gmail.com>
2020-12-13 16:24:24 -0500

    commit tree 36cf5e3d3acfa348434da3858880fd379516cf52

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,14 @@
 // TODO: Create action creators as defined in tests
+export function action(){
+return{
+    addQuote: (quote) => ({type: "ADD_QUOTE",
+    quote: quote,}),
+    removeQuote: (Id) => ({
+            type: "REMOVE_QUOTE",
+            quoteId: Id
+        }),
+    upvoteQuote: (Id) => ({type: "UPVOTE_QUOTE", quoteId: Id}),
+    downvoteQuote: (id) => ({type: "DOWNVOTE_QUOTE",
+    quoteId: id})
+ }
+}
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,16 @@
 export default (state = [], action) => {
-  return state;
+  switch (action.type){
+    case "ADD_QUOTE":
+      return {...state.concat(action.quote)}
+      break;
+    case "REMOVE_QUOTE":
+      const index = state.findIndex((quote) => quote.id === action.quoteId)
+      return {...state.slice(0, index).concat(...state.slice(index+1))}
+    case "UPVOTE_QUOTE":
+
+    case "DOWNVOTE_QUOTE":
+
+      default:
+      return state;
+  }
 }
